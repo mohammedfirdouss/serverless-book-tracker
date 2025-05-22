1974cc61b2fcfb59bfd63aa2b7c37e4b3e750f9f
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-05-22 11:50:18 +0000

    refactor: restructure by removing old stack files and adding new API stack

--- book-tracker-cdk/bin/book-tracker-api.ts
+++ book-tracker-cdk/bin/book-tracker-api.ts
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { BookTrackerCdkStack } from '../lib/book-tracker-cdk-stack';
+import { BookTrackerCdkStack } from '../lib/book-tracker-api-stack';
 
 const app = new cdk.App();
-new BookTrackerCdkStack(app, 'BookTrackerCdkStack', {
+new BookTrackerCdkStack(app, 'BookTrackerApiStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -11,10 +12,6 @@ new BookTrackerCdkStack(app, 'BookTrackerCdkStack', {
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
