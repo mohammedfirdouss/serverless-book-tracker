c8859b1d1f1669cda4f99dbe1621cbdf58cf488d
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-05-22 11:31:02 +0000

    feat: initialize book-tracker-cdk project with TypeScript, Jest, and AWS CDK

---
+++ book-tracker-cdk/test/book-tracker-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BookTrackerCdk from '../lib/book-tracker-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/book-tracker-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BookTrackerCdk.BookTrackerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
