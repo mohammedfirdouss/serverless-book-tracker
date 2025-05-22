07d55fe72bb07cc3eb8df2f9d3bb68f19cefdcf0
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-05-22 13:22:37 +0000

    chore: update package.json to add linting and formatting scripts, and update dependencies

---
+++ book-tracker-cdk/eslint.config.mjs
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  // Node config files override
+  {
+    files: ["*.config.js", "*.config.mjs", "jest.config.js"],
+    languageOptions: { globals: globals.node },
+  },
+  // Main project files
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+]);
--- book-tracker-cdk/test/book-tracker-api.test.ts
+++ book-tracker-cdk/test/book-tracker-api.test.ts
@@ -4,14 +4,13 @@
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/book-tracker-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new BookTrackerCdk.BookTrackerCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
-
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new BookTrackerCdk.BookTrackerCdkStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
 });
